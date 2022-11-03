3c5e5c388d33ee521379fcdcfd4121cc70ebdf28
Александр Львов <79278149+jullic@users.noreply.github.com>
2022-11-03 19:37:34 +0300

    start

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
